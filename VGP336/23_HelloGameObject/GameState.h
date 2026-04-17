ec751dc62f47c191275ddea162873b818b98204f
Ghuztavo <ghuztavo2002@gmail.com>
2026-04-16 21:58:01 -0700

    GameWorld, GameObjects, Components and services added

---
+++ VGP336/23_HelloGameObject/GameState.h
@@ -0,0 +1,17 @@
+#pragma once
+
+#include <SpEngine/Inc/SpEngine.h>
+
+class GameState : public SpEngine::AppState 
+{
+public:
+	void Initialize() override;
+	void Terminate() override;
+	void Update(float deltaTime) override;
+	void Render() override;
+	void DebugUI() override;
+
+private:
+	SpEngine::GameWorld mGameWorld;
+	SpEngine::CameraComponent* mCameraComponent = nullptr;
+};
